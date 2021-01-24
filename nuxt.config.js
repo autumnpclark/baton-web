adc1c6412c31f49c034276384b8ee447485355ca
Autumn Clark <autumnpclark@gmail.com>
2021-01-23 18:37:50 -0700

    fixed base

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,10 @@ export default {
     dir: 'docs'
   },
 
+  router: {
+    base: '/baton-web/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - relay',
